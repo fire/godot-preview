b286586e68fd434f8295d4afe7e6da9a91cf170e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2019-04-04 19:25:26 -0700

    Shrink output and delete output folder before running output images per frame.

--- camera.gd
+++ camera.gd
@@ -3,21 +3,43 @@
 
 extends Camera
 
-var images = []
+var images = {}
+var frame_acc = 0
 
 func _ready() -> void:
 #	if OS.has_feature("standalone"):
 #		get_viewport().msaa = Viewport.MSAA_2X
 #		get_viewport().size *= 4
 	var directory: = Directory.new()
-	directory.make_dir("user://render")
+	directory.list_dir_begin()
+	if directory.dir_exists("user://actual"):
+		rm_contents("user://actual")
+	directory.make_dir("user://actual")
 
-func _process(delta: float) -> void:
+
+func rm_contents(path):
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while (file_name != ""):
+#			if dir.current_is_dir():
+#				print("Found directory: " + file_name)
+#			else:
+#				print("Removed file: " + file_name)
+			file_name = dir.get_next()
+			dir.remove(file_name)
+	else:
+		print("An error occurred when trying to access the path.")
+
+func _process(delta):
 #	if OS.has_feature("standalone"):
 	# The first frame is always black, there's no point in saving it
 	if Engine.get_frames_drawn() == 0:
 		return
-
+	if Engine.get_frames_drawn() <= frame_acc + 60:
+		return
+	frame_acc = Engine.get_frames_drawn()
 	print("Rendering frame {frame}...".format({
 				frame = Engine.get_frames_drawn(),
 			})
@@ -34,12 +56,10 @@ func _process(delta: float) -> void:
 
 	# The viewport must be flipped to match the rendered window
 	image.flip_y()
-	images.push_back(image)
+	images[Engine.get_frames_drawn()] = image
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	var count = 0
 	for i in images:
-		var image : Image = i
-		var error := image.save_png("user://render/" + str(count) + ".png")
-		count += 1
+		var image : Image = images[i]
+		var error := image.save_png("user://actual/" + str(i) + ".png")
 	get_tree().quit()
